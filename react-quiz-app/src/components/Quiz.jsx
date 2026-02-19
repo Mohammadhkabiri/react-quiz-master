d4c8bface984bfdd1e72e7f0455536a908593867
Mohammad <m.kabiri.s1385@gmail.com>
2026-02-19 06:01:29 +0330

    Add timer to Quiz questions

--- react-quiz-app/src/components/Quiz.jsx
+++ react-quiz-app/src/components/Quiz.jsx
@@ -1,29 +1,51 @@
 import Questions from "../../questions";
-import { useState } from "react";
+import quizComplete from "../assets/quiz-complete.png";
+import { useState, useCallback } from "react";
+import QuizTimer from "./QuizTimer";
+
+const timeOut = 10000;
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-
-  function handleSelectAnswer(selectedAnswer) {
+  const isQuizFinished = activeQuestionIndex === Questions.length;
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+
+  if (isQuizFinished) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="Quiz complete image" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
   }
+
+  const shuffledAnswers = [...Questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
-    <div className="quiz">
-      <div className="question">
+    <div id="quiz"> 
+      <div id="question"> 
+        <QuizTimer
+          key={activeQuestionIndex}
+          timeOut={timeOut}
+          onTimeOut={handleSkipAnswer}
+        />
         <h2>{Questions[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {Questions[activeQuestionIndex].answers.map((answer) => {
-            return (
-              <li key={answer} className="answer">
-                <button onClick={() => handleSelectAnswer(answer)}>
-                  {answer}
-                </button>
-              </li>
-            );
-          })}
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ react-quiz-app/src/components/QuizTimer.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export default function QuizTimer({ timeOut, onTimeOut }) {
+  const [remainingTime, setRemainingTime] = useState(timeOut);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeOut, timeOut);
+    return () => clearTimeout(timer);
+  }, [timeOut, onTimeOut]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <progress id="question-time" value={remainingTime} max={timeOut} />;
+}
